35bf8aa86022e330a28950773fb77f231548789b
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2022-05-31 13:24:17 -0300

    realocação de arquivos

--- src/main/java/com/home/gerenciadordeprojetos/repositories/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.home.gerenciadordeprojetos.repositories;
-
-import com.home.gerenciadordeprojetos.model.Project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long>{
-    
-}
